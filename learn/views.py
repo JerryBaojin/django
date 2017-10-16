b4afb737deae8ea408c93356deb825e1caa0a99a
root <root@localhost.localdomain>
2017-10-16 14:04:35 +0800

    py

---
+++ learn/views.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+from django.http import HttpResponse
+from django.shortcuts import render
+ 
+def index(request):
+    return HttpResponse(u"hellow py!")
+
+
+def add(request):
+	a=request.GET['a']
+	b=request.GET['b'] 
+	c=int(a)+int(b)
+	return HttpResponse(str(c)) #default ways
+
+def add2(request,a,b):
+	c=int(a)+int(b)
+	return HttpResponse(str(c))
