eb914593b4502e9361e0ac5753c8a5521d4f1757
FlyingBugger <694767812@qq.com>
2018-02-04 23:17:40 +0800

    normal

--- firstDjango/settings.py
+++ firstDjango/settings.py
@@ -117,5 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "common_static"),
+ #   '/path/to/others/static/',  # 用不到的时候可以不写这一行
+)
